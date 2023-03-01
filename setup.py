1326f087c23422486f4bb55ca3cc1fb98e40232b
hassaanalansary <hassaanalansary@yahoo.com>
2023-03-01 16:59:56 +0200

    move long_description to setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,8 @@ from os import path
 from setuptools import setup
 
 
-# this_directory = path.abspath(path.dirname(__file__))
-# with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
-#     long_description = f.read()
-# if __name__== "__main__":
-#     setup(long_description=long_description, long_description_content_type="text/markdown")
-
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 if __name__ == "__main__":
-    setup()
+    setup(long_description=long_description, long_description_content_type="text/markdown")
